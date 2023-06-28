d2663d27343be438a9450e6845a6e3ff613b8d7a
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-28 23:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+// Get the element with ID "level"
+const levelElement = document.getElementById("level");
+
+// Initialize the level counter to 1
+let level = 1;
+
+// Traverse up the DOM hierarchy and count the levels
+let parent = levelElement.parentNode;
+while (parent.tagName !== "BODY") {
+  level++;
+  parent = parent.parentNode;
+}
+
+// Display the final answer using alert
+alert("The level of the element is: " + level);
+```
+
+This JavaScript code gets the element with the ID "level" using the `getElementById` method, initializes the level counter to 1, and traverses up the DOM hierarchy using the `parentNode` property of the element until it reaches the `<body>` element. At each level, the level counter is incremented. Finally, the code displays the final answer using the `alert` function with the level value.
+
+Note that the ID of the element should not be changed.
